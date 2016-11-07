565f6f1e2ad92d89c8b9bf0207011abc58d7df49
Morten <Morten@DESKTOP-4EOPIJR>
2016-11-07 12:54:40 +0100

    Add project files.

---
+++ GettingReal/TimeCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    public class TimeCalculator
+    {
+
+        public readonly static DateTime DateToday = DateTime.Today;
+
+        public string calculateAgeFromBirthdate(DateTime Birthdate)
+        {
+
+            DateTimeSpan dateSpan = DateTimeSpan.CompareDates(Birthdate, DateToday);
+            string AgeFormatted = "Years: " + dateSpan.Years + " Months " + dateSpan.Months + " Days " + dateSpan.Days;
+            return AgeFormatted;
+
+        }
+
+        public DateTime calulateBirthdateFromAge(DateTime Age)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
---
+++ GettingReal/TimeCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GettingReal
+{
+
+    [TestClass]
+    public class TimeCalculatorTests
+    {
+
+        [TestMethod]
+        public void returnTodaysDate()
+        {
+
+            Assert.AreEqual(DateTime.Today, TimeCalculator.DateToday);
+
+        }
+
+        [TestMethod]
+        public void canCalculateAgeFromBirthdate()
+        {
+
+            TimeCalculator TimeCalculator = new TimeCalculator();
+            DateTime Birthdate = new DateTime(2014, 10, 1);
+            DateTime DateToday = DateTime.Today;
+
+            string ageFormatted = TimeCalculator.calculateAgeFromBirthdate(Birthdate);
+
+            DateTimeSpan dateSpan = DateTimeSpan.CompareDates(Birthdate, DateToday);
+            string dateSpanFormatted = "Years: " + dateSpan.Years + " Months " + dateSpan.Months + " Days " + dateSpan.Days;
+
+            Assert.AreEqual(dateSpanFormatted, ageFormatted);
+
+        }
+
+    }
+}

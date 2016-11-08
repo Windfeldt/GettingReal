4e228a58088af81bfb29c4ea79d1cd78bf0b6658
Morten <Morten@DESKTOP-4EOPIJR>
2016-11-08 10:32:34 +0100

    Update

--- GettingReal/Cow.cs
+++ GettingReal/Cow.cs
@@ -14,17 +14,36 @@ namespace GettingReal
         public string name { get; set; }
         public DateTime birthdate { get; set; }
         public string age { get; set; }
-        public double birthWeight { get; set; }
+        public double birthweight { get; set; }
         public double currentWeight { get; set; }
         public DateTime deliveryDate { get; set; }
         public DateTime maturationDate { get; set; }
 
-        public void calculateAgeFromBirthdate(DateTime Birthdate)
+        public void calculateAgeFromBirthdate()
         {
 
-            DateTimeSpan dateSpan = DateTimeSpan.CompareDates(Birthdate, DateToday);
+            DateTimeSpan dateSpan = DateTimeSpan.CompareDates(this.birthdate, DateToday);
             string AgeFormatted = "Years: " + dateSpan.Years + " Months: " + dateSpan.Months + " Days: " + dateSpan.Days;
             this.age = AgeFormatted;
+
+        }
+
+        public void calculateWeightFromBirthdateAndBirthweight()
+        {
+
+            // Udregn antal dage fra kalvens fødselsdag til dags dato.
+            double days = (DateToday - this.birthdate).TotalDays;
+            double daysOver2Years = 0;
+
+            // Hvis kalven er ældre end 2 år, så læg de overskydende dage over i "daysOver2Years" og træk dem fra "days".
+            // På denne måde kan bruge forskellige udregninger på de forskellige dage.
+            if (days - 730 > 0)
+            {
+                daysOver2Years = days - 730;
+                days = days - daysOver2Years;
+            }
+
+            this.currentWeight = this.birthweight + days * 0.75 + daysOver2Years * 1.5;
 
         }
 
--- GettingReal/CowTests.cs
+++ GettingReal/CowTests.cs
@@ -25,14 +25,39 @@ namespace GettingReal
         {
 
             Cow cow = new Cow();
-            DateTime Birthdate = new DateTime(2014, 10, 1);
-            cow.calculateAgeFromBirthdate(Birthdate);
+            cow.birthdate = new DateTime(2014, 10, 1);
+            cow.calculateAgeFromBirthdate();
 
             DateTime DateToday = DateTime.Today;
+            DateTime Birthdate = new DateTime(2014, 10, 1);
             DateTimeSpan dateSpan = DateTimeSpan.CompareDates(Birthdate, DateToday);
             string dateSpanFormatted = "Years: " + dateSpan.Years + " Months: " + dateSpan.Months + " Days: " + dateSpan.Days;
 
             Assert.AreEqual(dateSpanFormatted, cow.age);
+
+        }
+
+        [TestMethod]
+        public void canCalculateWeightFromBirthdate()
+        {
+
+            Cow cow = new Cow();
+            cow.birthdate = new DateTime(2014, 9, 8);
+            cow.birthweight = 45;
+            cow.calculateWeightFromBirthdateAndBirthweight();
+
+            double days = (DateTime.Today - cow.birthdate).TotalDays;
+            double daysOver2Years = 0;
+
+            if (days - 730 > 0)
+            {
+                daysOver2Years = days - 730;
+                days = days - daysOver2Years;
+            }
+
+            double birthweight = cow.birthweight + days * 0.75 + daysOver2Years * 1.5;
+
+            Assert.AreEqual(birthweight, cow.currentWeight);
 
         }
 

28f72717c72a80d3b8eb50222e9977aa8562dccd
Windfeldt <Windfeldt@DESKTOP-C7LMTCM>
2016-11-17 10:12:20 +0100

    GUI implementet

---
+++ GettingReal/TableAfKalve.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GettingReal
+{
+    public partial class TableAfKalve : Form
+    {
+        public TableAfKalve()
+        {
+            InitializeComponent();
+        }
+    }
+}
